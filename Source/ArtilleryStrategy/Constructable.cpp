e8af984163e0efa97c88311d1f34f4150645b53e
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-02-09 21:11:34 +0500

    Initial commit

---
+++ Source/ArtilleryStrategy/Constructable.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Constructable.h"
+
+// Add default functionality here for any IConstructable functions that are not pure virtual.
---
+++ Source/ArtilleryStrategy/ConstructableBuilding.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ConstructableBuilding.h"
+
+// Add default functionality here for any IConstructableBuilding functions that are not pure virtual.
