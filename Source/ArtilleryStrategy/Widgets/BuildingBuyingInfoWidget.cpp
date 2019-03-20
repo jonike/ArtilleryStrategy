6bc581954a9863f9d94e28bee9961f4dadd31532
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-03-20 17:39:04 +0500

    Add filling building data for building info widget

--- Source/ArtilleryStrategy/Widgets/BuildingBuyingInfoWidget.cpp
+++ Source/ArtilleryStrategy/Widgets/BuildingBuyingInfoWidget.cpp
@@ -2,11 +2,25 @@
 
 
 #include "BuildingBuyingInfoWidget.h"
+#include "Structs/BuildingData.h"
 
 void UBuildingBuyingInfoWidget::Setup(FBuildingData& BuildingData)
 {
+	if (const auto IconWidget = GetIconWidget())
+	{
+		IconWidget->SetBrushFromTexture(BuildingData.Icon);
+	}
+	if (const auto NameWidget = GetNameWidget())
+	{
+		NameWidget->SetText(FText::FromName(BuildingData.FriendlyName));
+	}
+	if (const auto CostWidget = GetCostWidget())
+	{
+		CostWidget->SetText(FText::AsNumber(BuildingData.Cost));
+	}
 }
 
 void UBuildingBuyingInfoWidget::SpawnBuilding()
 {
+	// TODO: add logic for spawning buildings
 }
