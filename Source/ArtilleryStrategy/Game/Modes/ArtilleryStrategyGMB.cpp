4106804f9a8209bd11b2cbd0ccb9ebb9c4ea83ef
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-03-20 20:25:06 +0500

    Add HUD class

--- Source/ArtilleryStrategy/Game/Modes/ArtilleryStrategyGMB.cpp
+++ Source/ArtilleryStrategy/Game/Modes/ArtilleryStrategyGMB.cpp
@@ -2,10 +2,16 @@
 
 #include "ArtilleryStrategyGMB.h"
 #include "Components/GridGenerator.h"
+#include "Player/Controllers/DefaultPlayerController.h"
+#include "Player/States/DefaultPlayerState.h"
+#include "Player/HUD/DefaultHUD.h"
 
 AArtilleryStrategyGMB::AArtilleryStrategyGMB()
 {
 	GridGenerator = CreateDefaultSubobject<UGridGenerator>(TEXT("Grid Generator"));
+	PlayerControllerClass = ADefaultPlayerController::StaticClass();
+	PlayerStateClass = ADefaultPlayerState::StaticClass();
+	HUDClass = ADefaultHUD::StaticClass();
 }
 
 void AArtilleryStrategyGMB::BeginPlay()
