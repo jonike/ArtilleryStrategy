78954447b3efd15a44c52eb90da3b8508c1245b9
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-03-23 22:19:21 +0500

    Add component for automatic capital placement on game launch

---
+++ Source/ArtilleryStrategy/Components/CapitalPlacementGenerator.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CapitalPlacementGenerator.h"
+#include "Engine/World.h"
+#include "Interfaces/OwnerController.h"
+#include "Interfaces/Building.h"
+#include "Actors/CapitalBuilding.h"
+#include "Interfaces/GridPlatform.h"
+
+// Sets default values for this component's properties
+UCapitalPlacementGenerator::UCapitalPlacementGenerator()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UCapitalPlacementGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+}
+
+void UCapitalPlacementGenerator::PlaceCapital(TScriptInterface<IOwnerController> Controller) const
+{
+	check(Controller);
+	const auto Capital = CreateCapitalBuilding(GetSpawnCell()->GetBuildingSpawnLocation());
+	SetupCapitalBuilding(Capital, Controller);
+}
+
+ACapitalBuilding* UCapitalPlacementGenerator::CreateCapitalBuilding(FVector Location) const
+{
+	const auto Spawned = GetWorld()->SpawnActor(CapitalActorClass, &Location);
+	return Cast<ACapitalBuilding>(Spawned);
+}
+
+TScriptInterface<IGridPlatform> UCapitalPlacementGenerator::GetSpawnCell() const
+{
+	// TODO: add implementation
+	return nullptr;
+}
+
+void UCapitalPlacementGenerator::SetupCapitalBuilding(ACapitalBuilding* Capital, TScriptInterface<IOwnerController> Controller) const
+{
+	Capital->SetOwnerController(Controller);
+}
+
+// Called every frame
+void UCapitalPlacementGenerator::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UCapitalPlacementGenerator::PlaceCapitalsForAll() const
+{
+	auto ControllerIterator = GetWorld()->GetControllerIterator();
+	while (ControllerIterator)
+	{
+		PlaceCapital(ControllerIterator->Get());
+		++ControllerIterator;
+	}
+}
---
+++ Source/ArtilleryStrategy/Components/CapitalPlacementGenerator.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CapitalPlacementGenerator.generated.h"
+
+class IOwnerController;
+class IBuilding;
+class IGridPlatform;
+class ACapitalBuilding;
+class AController;
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class ARTILLERYSTRATEGY_API UCapitalPlacementGenerator : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UCapitalPlacementGenerator();
+
+	// Called every frame
+	void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void PlaceCapitalsForAll() const;
+
+protected:
+	// Called when the game starts
+	void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = Capitals, meta = (MustImplement = "IBuilding"))
+	TSubclassOf<ACapitalBuilding> CapitalActorClass;
+
+	void PlaceCapital(TScriptInterface<IOwnerController> Controller) const;
+	ACapitalBuilding* CreateCapitalBuilding(FVector Location) const;
+	TScriptInterface<IGridPlatform> GetSpawnCell() const;
+	void SetupCapitalBuilding(ACapitalBuilding* Capital, TScriptInterface<IOwnerController> Controller) const;
+};
