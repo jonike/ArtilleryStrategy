aed4363286bea5f5f075d2e3a35f1c1a5d79c388
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-04-01 18:41:17 +0500

    Delete mistakenly added files

--- enc_temp_folder/6da7e96cca46e73e3172a2a31dc2ccd/ResourceWidget.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "ResourceWidget.h"
-
-void UResourceWidget::Fill(const FName& Name, const FResource& Resource)
-{
-	FillWidget(Name, Resource);
-}
