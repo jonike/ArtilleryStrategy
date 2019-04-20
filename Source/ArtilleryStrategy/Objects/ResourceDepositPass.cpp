cadd96707982a170cf5ef01609abb1ba025af342
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-04-20 07:37:18 +0500

    Add ResourceDepositPass class and setup corresponding generation pass

---
+++ Source/ArtilleryStrategy/Objects/ResourceDepositPass.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ResourceDepositPass.h"
+#include "Structs/Resource.h"
+#include "Structs/ResourceDeposit.h"
+#include "Interfaces/GridPlatform.h"
+#include "Structs/WorldParams.h"
+#include "TileMatrix.h"
+
+void UResourceDepositPass::GenerateWorld(FWorldParams& Params)
+{
+	CreateDeposits(Params);
+}
+
+FResourceHandle UResourceDepositPass::GetRandomResource() const
+{
+	const auto Index = FMath::RandRange(0, AvailableDeposits.Num() - 1);
+	return AvailableDeposits[Index];
+}
+
+int32 UResourceDepositPass::GetRandomResourceAmount(const FResource& Resource) const
+{
+	return FMath::RandRange(Resource.MinAmountSpawned, Resource.MaxAmountSpawned);
+}
+
+void UResourceDepositPass::CreateDeposits(const FWorldParams& Params) const
+{
+	for (size_t i = 0; i < DepositsAmount; i++)
+	{
+		FResourceDeposit ResourceDeposit;
+		ResourceDeposit.ResourceAmount.ResourceHandle = GetRandomResource();
+		ResourceDeposit.ResourceAmount.Amount = GetRandomResourceAmount(ResourceDeposit.ResourceAmount.GetResource(TEXT("Getting resource data for spawning deposits")));
+		GetTileForDeposit(Params)->SetResourceDeposits(ResourceDeposit);
+	}
+}
+
+TScriptInterface<IGridPlatform> UResourceDepositPass::GetTileForDeposit(const FWorldParams& Params) const
+{
+	const auto Row = FMath::RandRange(0, Params.TileMatrix->GetRows() - 1);
+	const auto Column = FMath::RandRange(0, Params.TileMatrix->GetColumns() - 1);
+	return Params.TileMatrix->Get(Row, Column);
+}
---
+++ Source/ArtilleryStrategy/Objects/ResourceDepositPass.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Interfaces/WorldGenerationPass.h"
+#include "Structs/ResourceHandle.h"
+#include "ResourceDepositPass.generated.h"
+
+class IGridPlatform;
+struct FResource;
+class ISpawnStrategy;
+/**
+ * 
+ */
+UCLASS()
+class ARTILLERYSTRATEGY_API UResourceDepositPass : public UDataAsset, public IWorldGenerationPass
+{
+	GENERATED_BODY()
+
+public:
+	void GenerateWorld(FWorldParams& Params) override;
+
+protected:
+	virtual FResourceHandle GetRandomResource() const;
+	virtual int32 GetRandomResourceAmount(const FResource& Resource) const;
+	virtual TScriptInterface<IGridPlatform> GetTileForDeposit(const FWorldParams& Params) const;
+
+private:
+	UPROPERTY(Category = "Resources", EditDefaultsOnly)
+	int DepositsAmount = 4;
+
+	UPROPERTY(Category = "Resources", EditDefaultsOnly)
+	TArray<FResourceHandle> AvailableDeposits;
+
+	void CreateDeposits(const FWorldParams& Params) const;
+};
