8a513b552250d3f7ce7ef4215669fd3998c88ef1
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-04-24 11:32:51 +0500

    Add resetting all limits on each start of the turn

--- Source/ArtilleryStrategy/Player/States/DefaultPlayerState.cpp
+++ Source/ArtilleryStrategy/Player/States/DefaultPlayerState.cpp
@@ -3,9 +3,28 @@
 #include "DefaultPlayerState.h"
 #include "Objects/ResourceStorage.h"
 #include "Objects/ResourceBuildingsManager.h"
+#include "Components/TurnProcessorComponent.h"
 
 ADefaultPlayerState::ADefaultPlayerState()
 {
 	Storage = CreateDefaultSubobject<UResourceStorage>(TEXT("Resource storage"));
 	ResourceBuildingsManager = CreateDefaultSubobject<UResourceBuildingsManager>(TEXT("Resource buildings manager"));
+}
+
+void ADefaultPlayerState::ReceiveOnTurnStarted()
+{
+	TurnLimits.ResetAllLimits();
+}
+
+void ADefaultPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADefaultPlayerState::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	const auto TurnProcessor = GetTurnProcessor(this);
+	TurnProcessor->OnTurnStarted.AddDynamic(this, &ADefaultPlayerState::ReceiveOnTurnStarted);
+	TurnProcessor->OnTurnEnded.AddDynamic(this, &ADefaultPlayerState::ReceiveOnTurnEnded);
 }
--- Source/ArtilleryStrategy/Player/States/DefaultPlayerState.h
+++ Source/ArtilleryStrategy/Player/States/DefaultPlayerState.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerState.h"
 #include "Interfaces/Wallet.h"
 #include "Structs/PlayerTurnLimits.h"
+#include "Interfaces/TurnDependent.h"
 #include "DefaultPlayerState.generated.h"
 
 class UResourceBuildingsManager;
@@ -14,17 +15,23 @@ class UResourceStorage;
  *
  */
 UCLASS()
-class ARTILLERYSTRATEGY_API ADefaultPlayerState : public APlayerState, public IWallet
+class ARTILLERYSTRATEGY_API ADefaultPlayerState : public APlayerState, public IWallet, public ITurnDependent
 {
 	GENERATED_BODY()
 
 public:
 	ADefaultPlayerState();
 
+	void ReceiveOnTurnStarted() override;
+
 	UFUNCTION(Category = "Wallet", BlueprintPure)
 	UResourceStorage* GetResourceWallet() const override { return Storage; }
 
 	auto GetResourceBuildingsManager() const { return ResourceBuildingsManager; }
+
+protected:
+	void BeginPlay() override;
+	void PostInitializeComponents() override;
 
 private:
 	UPROPERTY(Category = "Resources", EditAnywhere)
