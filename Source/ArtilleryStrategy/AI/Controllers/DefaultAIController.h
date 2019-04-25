45f9930612225bd53c3b4a2cad6b5bdad9c12c5b
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-04-25 20:39:24 +0500

    Add AI controller class

---
+++ Source/ArtilleryStrategy/AI/Controllers/DefaultAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "DefaultAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARTILLERYSTRATEGY_API ADefaultAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
