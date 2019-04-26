577b89928cee76e3d600276273a9c569537afb5d
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-04-26 21:32:41 +0500

    Add IOwnerController interface to AI controller

--- Source/ArtilleryStrategy/AI/Controllers/DefaultAIController.cpp
+++ Source/ArtilleryStrategy/AI/Controllers/DefaultAIController.cpp
@@ -3,6 +3,22 @@
 
 #include "DefaultAIController.h"
 #include "BehaviorTree/BehaviorTree.h"
+#include "Player/States/DefaultPlayerState.h"
+
+UMaterialInterface* ADefaultAIController::GetOwnerMaterial() const
+{
+	return OwnerMaterial;
+}
+
+UTexture2D* ADefaultAIController::GetOwnerIcon() const
+{
+	return OwnerIcon;
+}
+
+TScriptInterface<IWallet> ADefaultAIController::GetWallet() const
+{
+	return GetPlayerState<ADefaultPlayerState>();
+}
 
 void ADefaultAIController::BeginPlay()
 {
--- Source/ArtilleryStrategy/AI/Controllers/DefaultAIController.h
+++ Source/ArtilleryStrategy/AI/Controllers/DefaultAIController.h
@@ -4,17 +4,22 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Interfaces/OwnerController.h"
 #include "DefaultAIController.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class ARTILLERYSTRATEGY_API ADefaultAIController : public AAIController
+class ARTILLERYSTRATEGY_API ADefaultAIController : public AAIController, public IOwnerController
 {
 	GENERATED_BODY()
 
 public:
+	UMaterialInterface* GetOwnerMaterial() const override;
+	UTexture2D* GetOwnerIcon() const override;
+	TScriptInterface<IWallet> GetWallet() const override;
+
 protected:
 	void BeginPlay() override;
 
@@ -24,4 +29,10 @@ private:
 
 	UPROPERTY(Category = "Behavior", EditAnywhere)
 	UBlackboardData* BlackboardData;
+
+	UPROPERTY(Category = "Ownership", EditAnywhere)
+	UMaterialInterface* OwnerMaterial;
+
+	UPROPERTY(Category = "Ownership", EditAnywhere)
+	UTexture2D* OwnerIcon;
 };
