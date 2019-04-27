f04d950124cafe240e23e1ff62ee5fac62023f6e
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-04-27 22:28:15 +0500

    Add task for finding best acquired tile with resources on it

---
+++ Source/ArtilleryStrategy/AI/Tasks/FindBestTileForResourceBuilding.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FindBestTileForResourceBuilding.h"
+#include "Interfaces/GridPlatform.h"
+#include "Interfaces/CanBeOwned.h"
+#include "AIController.h"
+#include "Player/States/DefaultPlayerState.h"
+#include "Game/States/DefaultGS.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "Engine/World.h"
+#include "Structs/WorldParams.h"
+#include "Components/WorldGenerator.h"
+#include "Objects/TileMatrix.h"
+#include "Interfaces/OwnerController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UFindBestTileForResourceBuilding::UFindBestTileForResourceBuilding()
+{
+	ResultTile.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UFindBestTileForResourceBuilding, ResultTile), UGridPlatform::StaticClass());
+	ResultTile.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UFindBestTileForResourceBuilding, ResultTile), UCanBeOwned::StaticClass());
+}
+
+EBTNodeResult::Type UFindBestTileForResourceBuilding::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	const auto Controller = OwnerComp.GetAIOwner();
+	if (const auto PlayerState = Controller->GetPlayerState<ADefaultPlayerState>())
+	{
+		if (const auto GameState = OwnerComp.GetWorld()->GetGameState<ADefaultGS>())
+		{
+			const auto& WorldParams = GameState->GetGridGenerator()->GetWorldParams();
+			return GetBestResourceTile(OwnerComp, NodeMemory, WorldParams);
+		}
+	}
+	return EBTNodeResult::Failed;
+}
+
+EBTNodeResult::Type UFindBestTileForResourceBuilding::AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return EBTNodeResult::Failed;
+}
+
+void UFindBestTileForResourceBuilding::OnGameplayTaskActivated(UGameplayTask& Task)
+{
+
+}
+
+EBTNodeResult::Type UFindBestTileForResourceBuilding::GetBestResourceTile(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, const FWorldParams& Params)
+{
+	const auto SelfOwner = OwnerComp.GetAIOwner();
+	for (auto Row = 0; Row < Params.GetRows(); ++Row)
+	{
+		for (auto Column = 0; Column < Params.GetColumns(); ++Column)
+		{
+			const auto Tile = Params.TileMatrix->Get(Row, Column).GetObject();
+			if (const auto OwnedTile = Cast<ICanBeOwned>(Tile))
+			{
+				const auto TileOwner = OwnedTile->GetOwnerController().GetObject();
+				if (TileOwner == SelfOwner)
+				{
+					OwnerComp.GetBlackboardComponent()->SetValueAsObject(ResultTile.SelectedKeyName, Tile);
+					return EBTNodeResult::Succeeded;
+				}
+			}
+		}
+	}
+	return EBTNodeResult::Failed;
+}
---
+++ Source/ArtilleryStrategy/AI/Tasks/FindBestTileForResourceBuilding.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "FindBestTileForResourceBuilding.generated.h"
+
+struct FWorldParams;
+
+/**
+ * 
+ */
+UCLASS()
+class ARTILLERYSTRATEGY_API UFindBestTileForResourceBuilding : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UFindBestTileForResourceBuilding();
+
+	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	void OnGameplayTaskActivated(UGameplayTask& Task) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	FBlackboardKeySelector ResultTile;
+
+	EBTNodeResult::Type GetBestResourceTile(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, const FWorldParams& Params);
+};
