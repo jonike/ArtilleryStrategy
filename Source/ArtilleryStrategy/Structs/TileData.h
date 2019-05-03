1e733850607a0530844f37ac55a9eb9877bde7cf
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-05-03 21:33:40 +0500

    Add TileData struct for future implementation of advanced world generation

---
+++ Source/ArtilleryStrategy/Structs/TileData.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TileData.generated.h"
+
+/**
+ * Structure to hold all data about tile even before its created
+ */
+USTRUCT(BlueprintType)
+struct ARTILLERYSTRATEGY_API FTileData
+{
+	GENERATED_BODY()
+};
