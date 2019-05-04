83a22da846062c78a0c313ba2c2829175dfe2320
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2019-05-04 09:40:33 +0500

    Add TileCoordinates struct

---
+++ Source/ArtilleryStrategy/Structs/TileCoordinates.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TileCoordinates.h"
+
+FTileCoordinates::FTileCoordinates(const int Row, const int Column)
+{
+	Init(Row, Column);
+}
+
+void FTileCoordinates::Init(int R, int C)
+{
+	check(R >= 0);
+	check(C >= 0);
+	Row = R;
+	Column = C;
+}
+
+TArray<FTileCoordinates> FTileCoordinates::GetIncidentCoordinates(const int TotalRows, const int TotalColumns, const int Distance) const
+{
+	TArray<FTileCoordinates> Results;
+	for (auto R = Row - Distance; R <= Row + Distance; ++R)
+	{
+		for (auto C = Column - Distance; C <= Column+Distance; ++C)
+		{
+			if (!IsEqualToCurrent(R, C)
+				&& IsValidValue(R, TotalRows)
+				&& IsValidValue(C, TotalColumns))
+			{
+				Results.Emplace(R, C);
+			}
+		}
+	}
+	return Results;
+}
+
+bool FTileCoordinates::IsValidValue(const int Value, const int Max) const
+{
+	return Value >= 0 && Value < Max;
+}
+
+bool FTileCoordinates::IsEqualToCurrent(const int OtherRow, const int OtherColumn) const
+{
+	return Row == OtherRow && Column == OtherColumn;
+}
---
+++ Source/ArtilleryStrategy/Structs/TileCoordinates.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TileCoordinates.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct ARTILLERYSTRATEGY_API FTileCoordinates
+{
+	GENERATED_BODY()
+
+public:
+	FTileCoordinates() = default;
+	FTileCoordinates(int Row, int Column);
+
+	void Init(int R, int C);
+	TArray<FTileCoordinates> GetIncidentCoordinates(int TotalRows, int TotalColumns, int Distance) const;
+
+private:
+	int Row;
+	int Column;
+
+	bool IsValidValue(int Value, int Max) const;
+	bool IsEqualToCurrent(int OtherRow, int OtherColumn) const;
+};
